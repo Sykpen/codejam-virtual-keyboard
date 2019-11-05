dc85edd2d7a87a90ae2b3d3ef0d6c96595f18512
Sykpen <ser10potap@yandex.ru>
2019-11-05 23:35:22 +0300

    add keaboard and small deligation function

--- script.js
+++ script.js
@@ -7,11 +7,59 @@ let row = document.createElement('div');
 row.classList.add('row');
 mainwrapper.append(row);
 
-for(let i = 0;i < 14;i++){
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    row.append(cell);  
-}
+let keyLayout = [
+    "~","1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-","+", "back",
+    "tab","q", "w", "e", "r", "t", "y", "u", "i", "o", "p","[","]","/",
+    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l",";","'", "enter",
+    "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?","↑","shift",
+    "cntrl","win","alt","space","alt","win","←","↓","→","cntrl"
+];
 
-}   
+keyLayout.forEach(function keyKreation(key){
+    let keyElement = document.createElement("div");
+    keyElement.append(key);
+    keyElement.classList.add("cell");
+    row.append(keyElement);
+    
+    switch(key){
+        case "back":
+        keyElement.classList.add("big");
+    break;
+    case "tab":
+        keyElement.classList.add("big");
+    break;
+    case "caps":
+        keyElement.classList.add("big");
+    break;
+    case "enter":
+        keyElement.classList.add("big");
+    break;
+    case "shift":
+        keyElement.classList.add("big");
+    break;
+    case "space":
+        keyElement.classList.add("space");
+    break;          
+    }  
+});
+
+mainwrapper.addEventListener("click", function(){
+    let target = event.target.closest('div');
+    let kees = document.getElementsByClassName('cell');
+    kees.style.backgroundColor = '';   
+    target.classList.add("selected");
+});
+	
+
+
+
+}  
 createmain();
+
+
+
+// for(let i = 0;i < 14;i++){
+//     let cell = document.createElement('div');
+//     cell.classList.add('cell');
+//     row.append(cell);  
+// }
